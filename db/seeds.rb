a69bb493955a7123142aa33adc85ce4155fe49b4
Tiago Oliveirinha <tiago.oliveirinha1@gmail.com>
2021-04-24 15:35:04 +0100

    exercise done: models, controllers, views, routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,24 @@
 puts 'cleaning up database'
 Restaurant.destroy_all
+Review.destroy_all
 puts 'database is clean'
-puts 'creating restaurants'
+puts 'creating restaurants with reviews!'
 
 10.times do
-  Restaurant.create(
+  restaurant = Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
-    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164
   )
+
+  rand(1..10).times do
+    Review.create(
+      rating: rand(1..5),
+      content: Faker::Restaurant.review,
+      restaurant: restaurant
+    )
+  end
 end
 
-puts 'restaurants created'
+puts 'restaurants + reviews created'
